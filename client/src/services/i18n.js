80cb95602489bb8de33ef704c7696f8f23aeb9d4
Gwagsi Glenn <gwagsiglenn@gmail.com>
2021-01-04 13:31:51 +0100

    browerlist update

--- client/src/services/i18n.js
+++ client/src/services/i18n.js
@@ -1,12 +1,7 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 import detector from "i18next-browser-languagedetector";
-import Backend from 'i18next-http-backend';
-
-
-
-
-
+import Backend from "i18next-http-backend";
 
 i18n
   .use(detector)
@@ -15,17 +10,17 @@ i18n
   // connect with React
   .use(initReactI18next)
   // for all options read: https://www.i18next.com/overview/configuration-options
-    
+
   .init({
     debug: true,
-  
+
     fallbackLng: "en", // use en if detected lng is not available
-    whitelist: ['en', 'fr', 'es'],
+    whitelist: ["en", "fr", "es"],
     keySeparator: false, // we do not use keys in form messages.welcome
 
     interpolation: {
-      escapeValue: false // react already safes from xss
-    }
+      escapeValue: false, // react already safes from xss
+    },
   });
 
 export default i18n;
